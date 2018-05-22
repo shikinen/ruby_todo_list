57986c11dcd901e88ac6fbbde63aa1dc1a9389be
Rafal Miodowski <rafalmiodowski@gmail.com>
2018-05-22 11:59:13 +0200

    Add basic class structure

---
+++ list.rb
@@ -0,0 +1,28 @@
+class List
+	def create_list
+	end
+
+	def add_task
+	end
+
+	def show_tasks
+	end
+
+	def read_task
+	end
+
+	def write_list_to_file
+	end
+
+	def delete_task
+	end
+
+	def update_task
+	end
+end
+
+
+class Task
+	def create_task_item
+	end
+end
